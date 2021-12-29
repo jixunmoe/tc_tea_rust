672091e12956b483564de4328f2d8155a6a87c5b
Jixun Wu <jixun.moe@gmail.com>
2021-12-29 00:22:31 +0000

    fix: test from cli

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! Notably, it uses a different round number and uses a "tweaked" CBC mode.
 
 mod stream_ext;
-mod tc_tea;
+mod tc_tea_public;
 mod tc_tea_internal;
 
-pub use tc_tea::*;
+pub use tc_tea_public::*;
